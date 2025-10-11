14ce3f57a4f441a5d58111b40b844dbd1563e799
João Paulo <jpaulorufato@hotmail.com>
2025-10-11 03:51:06 -0300

    Adiciona eventos

---
+++ Eventos/Evento.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MoodPlusAPI.Eventos
+{
+    [BsonIgnoreExtraElements]
+    public class Evento
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string EmpresaId { get; set; }
+        public DateTime? Inicio { get; set; }
+        public DateTime? Fim { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
